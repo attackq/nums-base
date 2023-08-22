e0762e8d7c19f8816dbe1fec8a23ca9300065da8
Alexander Panasik <rastpk@gmail.com>
2023-08-22 23:07:03 +0300

    bugs fixed

--- server/index.js
+++ server/index.js
@@ -1,30 +1,27 @@
-const app = require('express')();
-const httpServer = require('http').createServer(app);
+const app = require("express")();
+const httpServer = require("http").createServer(app);
 
 const io = require("socket.io")(httpServer, {
-    cors: {
-        origin: "http://localhost:4200",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+  },
 });
 
 const port = 5005;
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('message', (message) => {
-        console.log(message);
-        console.log(socket.id)
-        io.emit('message', `${socket.id.substr(0, 2)}: ${message}`)
-    });
+  socket.on("message", (message) => {
+    console.log(message);
+    console.log(socket.id);
+    io.emit("message", `${socket.id.substr(0, 2)}: ${message}`);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('a user disconnected')
-    });
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
 });
 
-httpServer.listen(port, () => console.log(`Listening on port ${port}`))
-
-
-
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
